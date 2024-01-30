3440b19c7f25617619c1fcb566efd75114024aea
JulieMontoux <julie.montoux@epsi.fr>
2024-01-30 23:20:17 +0100

    préparation authentification JWT

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from authentication.views import *
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -40,5 +41,4 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
          name='schema-swagger-ui'),  
     path('', include('frontend.urls')),
-  
 ]
