3d09962a6bcb7340c474752e027b5cbcd730d430
JulieMontoux <julie.montoux@epsi.fr>
2024-01-31 23:02:08 +0100

    essaie jwt
    a revoir, rien n'est fini

--- api_project/api_app/views.py
+++ api_project/api_app/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -8,6 +9,39 @@ from .serializers import (
     AlbumSerializer, GenreSerializer, ArtistSerializer,
     SongSerializer, UserSerializer
 )
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_jwt.settings import api_settings
+from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.serializers import VerifyJSONWebTokenSerializer
+from rest_framework_jwt.utils import jwt_response_payload_handler
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_jwt.settings import api_settings
+from django.contrib.auth.models import User
+from rest_framework_jwt.views import ObtainJSONWebToken
+
+class CustomJSONWebToken(ObtainJSONWebToken):
+    def post(self, request, *args, **kwargs):
+        self.request = request  
+        response = super().post(request, *args, **kwargs)
+        if response.status_code == status.HTTP_200_OK:
+            user = User.objects.get(username=request.data['username'])
+            jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+            jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+            payload = jwt_payload_handler(user)
+            token = jwt_encode_handler(payload)
+            response.data['token'] = token
+
+        return response
+
 
 # GET - /api/albums
 @api_view(['GET'])
@@ -53,12 +87,32 @@ def user_signin(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+# Configuration de l'authentification JWT
+jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
 # POST - /api/users/login
 @api_view(['POST'])
 def user_login(request):
-    # Ajoutez votre logique d'authentification JWT ici
-    # Ceci est juste un espace réservé
-    return Response({'message': 'User login successful'}, status=status.HTTP_200_OK)
+    # Extraire les données de la requête
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    # Vérifier si le nom d'utilisateur et le mot de passe sont présents
+    if username is None or password is None:
+        return Response({'error': 'Veuillez fournir un nom d\'utilisateur et un mot de passe'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    # Utiliser la vue obtain_jwt_token pour obtenir le token
+    response = obtain_jwt_token(request)
+    
+    # Vérifier si l'authentification a réussi
+    if response.status_code == status.HTTP_200_OK:
+        # Ajouter des informations supplémentaires au résultat si nécessaire
+        user = User.objects.get(username=username)
+        # Ajouter d'autres informations à la réponse si nécessaire
+
+    return response
 
 # POST - /api/albums
 @api_view(['POST'])
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from datetime import timedelta
+import datetime
 from pathlib import Path
 import os
 
@@ -29,7 +30,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,53 +40,29 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt.token_blacklist',
+    'rest_framework_simplejwt',
     'api_app',
     'drf_yasg',
     'frontend',
-    'authentication',
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     ),
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
+    #     'rest_framework.authentication.SessionAuthentication',
+    #     'rest_framework.authentication.BasicAuthentication',
+    # ],
 }
 
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=180),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=50),
-    'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': True,
-    'UPDATE_LAST_LOGIN': False,
+# JWT_AUTH = {
+#     'JWT_ALLOW_REFRESH': True,
+#     'JWT_EXPIRATION_DELTA': datetime.timedelta(days=1),
+# }
 
-    'ALGORITHM': 'HS256',
-
-    'VERIFYING_KEY': None,
-    'AUDIENCE': None,
-    'ISSUER': None,
-    'JWK_URL': None,
-    'LEEWAY': 0,
-
-    'AUTH_HEADER_TYPES': ('Bearer',),
-    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
-    'USER_ID_FIELD': 'id',
-    'USER_ID_CLAIM': 'user_id',
-    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
-
-    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-    'TOKEN_TYPE_CLAIM': 'token_type',
-    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
-
-    'JTI_CLAIM': 'jti',
-
-    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
-}
+# AUTH_USER_MODEL= 'api_app.CustomUser'
 
 SWAGGER_SETTINGS = {
     'SECURITY_DEFINITIONS': {
